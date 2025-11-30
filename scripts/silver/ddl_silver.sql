cdc538c3ce28910b1eab4b59d05f221913898742
Nitin Singh Bisht <nikhilbisht26@gmail.com>
2025-11-30 12:47:49 +0530

    silver layer done

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,60 @@
+
+DROP TABLE IF EXISTS silver.ma_artist_genre;
+DROP TABLE IF EXISTS silver.ma_track;
+DROP TABLE IF EXISTS silver.ma_genre;
+DROP TABLE IF EXISTS silver.ma_album;
+DROP TABLE IF EXISTS silver.ma_artist;
+
+CREATE TABLE silver.ma_artist(
+    artist_id SERIAL,
+	artist_name VARCHAR(200) NOT NULL,
+	artist_popularity INT NOT NULL,
+	artist_followers INT NOT NULL,
+	CONSTRAINT ma_artist_pkey
+	PRIMARY KEY(artist_id)
+);
+
+
+CREATE TABLE silver.ma_album(
+    album_id VARCHAR(30) NOT NULL,
+	album_name VARCHAR(200) NOT NULL,
+	album_release_date DATE NOT NULL,
+	album_total_tracks INT NOT NULL,
+	album_type VARCHAR(30) NOT NULL,
+	CONSTRAINT ma_album_pkey
+	PRIMARY KEY(album_id)
+);
+
+
+CREATE TABLE silver.ma_genre(
+    genre_id SERIAL NOT NULL,
+	genre VARCHAR(30) NOT NULL,
+	CONSTRAINT ma_genre_pkey
+	PRIMARY KEY(genre_id)
+);
+
+
+CREATE TABLE silver.ma_track(
+    track_id VARCHAR(30) NOT NULL,
+    track_name VARCHAR(200) NOT NULL,
+    track_number INT NOT NULL,
+    track_popularity INT NOT NULL,
+    track_duration_ms INT NOT NULL,
+    explicit BOOLEAN NOT NULL,
+    artist_id INT NOT NULL,
+    album_id  VARCHAR(30) NOT NULL,
+	CONSTRAINT fkey_ma_artist FOREIGN KEY(artist_id)
+	REFERENCES silver.ma_artist(artist_id),
+	CONSTRAINT fkey_album FOREIGN KEY(album_id)
+	REFERENCES silver.ma_album(album_id)
+);
+
+
+CREATE TABLE silver.ma_artist_genre(
+    artist_id INT NOT NULL,
+    genre_id INT NOT NULL,
+	CONSTRAINT fkey_ma_artist_bridge FOREIGN KEY(artist_id)
+	REFERENCES silver.ma_artist(artist_id),
+	CONSTRAINT fkey_ma_genre FOREIGN KEY(genre_id)
+	REFERENCES silver.ma_genre(genre_id)
+);
---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,170 @@
+CREATE OR REPLACE PROCEDURE silver.load_silver()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RAISE NOTICE 'Truncating Silver layer Tables';
+    TRUNCATE TABLE silver.ma_artist_genre,
+                   silver.ma_track,
+                   silver.ma_genre,
+                   silver.ma_album,
+                   silver.ma_artist;
+
+    RAISE NOTICE 'Inserting in ma_artist table';
+    INSERT INTO silver.ma_artist(artist_name, artist_popularity, artist_followers)
+    SELECT DISTINCT 
+        TRIM(artist_name),
+        artist_popularity::INT,
+        artist_followers::INT
+    FROM bronze.ma_spotify_track_2009_2023
+    WHERE artist_popularity IS NOT NULL 
+      AND artist_followers IS NOT NULL
+      AND artist_name IS NOT NULL
+    UNION
+    SELECT DISTINCT
+        TRIM(artist_name),
+        artist_popularity::INT,
+        artist_followers::INT
+    FROM bronze.ma_spotify_track_2025
+    WHERE artist_popularity IS NOT NULL 
+      AND artist_followers IS NOT NULL
+      AND artist_name IS NOT NULL;
+
+    RAISE NOTICE 'Inserting in ma_album table';
+    INSERT INTO silver.ma_album
+    SELECT DISTINCT
+        album_id,
+        album_name,
+        (DATE '1899-12-30' + album_release_date::INT),
+        album_total_tracks::INT,
+        album_type
+    FROM bronze.ma_spotify_track_2009_2023
+    WHERE album_id IS NOT NULL
+      AND album_name IS NOT NULL
+      AND album_total_tracks IS NOT NULL
+      AND album_type IS NOT NULL
+      AND album_release_date IS NOT NULL
+      AND album_release_date ~ '^[0-9]*$'
+    UNION
+    SELECT DISTINCT
+        album_id,
+        album_name,
+        (DATE '1899-12-30' + album_release_date::INT),
+        album_total_tracks::INT,
+        album_type
+    FROM bronze.ma_spotify_track_2025
+    WHERE album_id IS NOT NULL
+      AND album_name IS NOT NULL
+      AND album_total_tracks IS NOT NULL
+      AND album_type IS NOT NULL
+      AND album_release_date IS NOT NULL
+      AND album_release_date ~ '^[0-9]*$'
+    ON CONFLICT(album_id) DO NOTHING;
+
+    RAISE NOTICE 'Inserting in ma_genre table';
+    INSERT INTO silver.ma_genre (genre)
+    SELECT DISTINCT
+        TRIM(BOTH '''"' FROM REGEXP_SPLIT_TO_TABLE(
+            REPLACE(REPLACE(artist_genres, '[', ''), ']', ''),
+            '\s*,\s*'
+        )) AS genre
+    FROM bronze.ma_spotify_track_2009_2023
+    WHERE artist_genres IS NOT NULL AND artist_genres <> '[]'
+    UNION
+    SELECT DISTINCT
+        TRIM(BOTH '''"' FROM REGEXP_SPLIT_TO_TABLE(
+            REPLACE(REPLACE(artist_genres, '[', ''), ']', ''),
+            '\s*,\s*'
+        ))
+    FROM bronze.ma_spotify_track_2025
+    WHERE artist_genres IS NOT NULL AND artist_genres <> '[]';
+
+    RAISE NOTICE 'Inserting in ma_track table';
+    INSERT INTO silver.ma_track
+    SELECT
+        b.track_id,
+        TRIM(b.track_name),
+        b.track_number::INT,
+        b.track_popularity::INT,
+        b.track_duration_ms::INT,
+        b.explicit::BOOLEAN,
+        a.artist_id,
+        b.album_id
+    FROM bronze.ma_spotify_track_2009_2023 b
+    JOIN silver.ma_artist a
+        ON TRIM(b.artist_name) = a.artist_name
+    WHERE 
+          b.track_id IS NOT NULL AND TRIM(b.track_id) <> ''
+      AND b.track_name IS NOT NULL AND TRIM(b.track_name) <> ''
+      AND b.track_number IS NOT NULL
+      AND b.track_popularity IS NOT NULL
+      AND b.track_duration_ms IS NOT NULL
+      AND b.explicit IS NOT NULL
+      AND b.album_id IS NOT NULL AND TRIM(b.album_id) <> ''
+
+    UNION
+
+    SELECT
+        b.track_id,
+        TRIM(b.track_name),
+        b.track_number::INT,
+        b.track_popularity::INT,
+        ROUND(b.track_duration_min::NUMERIC * 60000)::INT,
+        b.explicit::BOOLEAN,
+        a.artist_id,
+        b.album_id
+    FROM bronze.ma_spotify_track_2025 b
+    JOIN silver.ma_artist a
+        ON TRIM(b.artist_name) = a.artist_name
+    WHERE 
+          b.track_id IS NOT NULL AND TRIM(b.track_id) <> ''
+      AND b.track_name IS NOT NULL AND TRIM(b.track_name) <> ''
+      AND b.track_number IS NOT NULL
+      AND b.track_popularity IS NOT NULL
+      AND b.track_duration_min IS NOT NULL
+      AND b.explicit IS NOT NULL
+      AND b.album_id IS NOT NULL AND TRIM(b.album_id) <> '';
+
+    RAISE NOTICE 'Inserting in ma_artist_genre table';
+    INSERT INTO silver.ma_artist_genre
+    SELECT DISTINCT
+        a.artist_id,
+        g.genre_id
+    FROM bronze.ma_spotify_track_2009_2023 b
+    JOIN silver.ma_artist a
+        ON TRIM(b.artist_name) = a.artist_name
+    CROSS JOIN LATERAL (
+        SELECT TRIM(BOTH '''"' FROM value) AS genre
+        FROM REGEXP_SPLIT_TO_TABLE(
+                REPLACE(REPLACE(b.artist_genres, '[', ''), ']', ''),
+                '\s*,\s*'
+        ) AS value
+    ) s
+    JOIN silver.ma_genre g
+        ON g.genre = s.genre
+    WHERE b.artist_genres IS NOT NULL AND b.artist_genres <> '[]'
+
+    UNION
+
+    SELECT DISTINCT
+        a.artist_id,
+        g.genre_id
+    FROM bronze.ma_spotify_track_2025 b
+    JOIN silver.ma_artist a
+        ON TRIM(b.artist_name) = a.artist_name
+    CROSS JOIN LATERAL (
+        SELECT TRIM(BOTH '''"' FROM value) AS genre
+        FROM REGEXP_SPLIT_TO_TABLE(
+                REPLACE(REPLACE(b.artist_genres, '[', ''), ']', ''),
+                '\s*,\s*'
+        ) AS value
+    ) s
+    JOIN silver.ma_genre g
+        ON g.genre = s.genre
+    WHERE b.artist_genres IS NOT NULL AND b.artist_genres <> '[]';
+
+    RAISE NOTICE 'Silver layer load complete.';
+
+END $$;
+
+
+CALL silver.load_silver();
